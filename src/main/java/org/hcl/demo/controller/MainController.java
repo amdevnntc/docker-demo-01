e3088bcdbd715c9e7c5357fbff0b2dd72ab1aac6
amdevnntc <amdevnntc@gmail.com>
2020-04-04 18:34:29 +0000

    final commit

--- src/main/java/org/hcl/demo/controller/MainController.java
+++ src/main/java/org/hcl/demo/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-	@GetMapping
+	@GetMapping("/")
 	public String showform() {
 		System.out.println("page loaded");
 		return "welcome spring boot page !";
