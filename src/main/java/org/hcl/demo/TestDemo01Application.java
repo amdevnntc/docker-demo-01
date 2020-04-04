c9947aa62267cfa53e4ff86cea7b4f75def792c0
amdevnntc <amdevnntc@gmail.com>
2020-04-04 17:28:18 +0000

    initial commit

---
+++ src/main/java/org/hcl/demo/TestDemo01Application.java
@@ -0,0 +1,13 @@
+package org.hcl.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDemo01Application.class, args);
+	}
+
+}
